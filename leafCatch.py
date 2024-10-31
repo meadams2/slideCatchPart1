54292e7a5bce99aab55336f464647d8750b73ba2
meadams2 <meadams2@bsu.edu>
2024-10-31 10:02:09 -0400

    Milestone2: WednesdayExists

--- leafCatch.py
+++ leafCatch.py
@@ -2,13 +2,19 @@
 CS120
 Slide and Catch Part 1"""
 import pygame, simpleGE
+class Wednesday(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Wednesday.png")
+        self.setSize(75, 75)
+        self.position = (320, 400)
         
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("FallScene.png")
-        
-        self.sprites = []
+        self.wednesday = Wednesday(self)
+        self.sprites = [self.wednesday]
 
 def main():
     game = Game()
